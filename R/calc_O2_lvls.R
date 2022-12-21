9b2c78d181cf9d8652be7f30d315a7887e13fd99
JARS3N <JARS3N@users.noreply.github.com>
2022-12-21 15:08:32 -0500

    Update calc_O2_lvls.R

--- R/calc_O2_lvls.R
+++ R/calc_O2_lvls.R
@@ -3,7 +3,7 @@ calc_O2_levels <- function(cal, lvls, coefs) {
   ppO2 <- foam::partial_pressure_ox(37)
   lvls <- select(lvls, Tick, Measure, Well, O2_CorrectedEmission)
   cal <- select(cal, Well, O2.CalEmission) %>%
-    mutate(F0 = O2.CalEmission * (ppO2 * x$O2_coefs$Ksv + 1))
+    mutate(F0 = O2.CalEmission * (ppO2 * coefs$Ksv + 1))
 
   xx <-  left_join(lvls, cal) %>%
     mutate(O2 =  (1 / coefs$Ksv) * ((F0 / O2_CorrectedEmission) - 1))
