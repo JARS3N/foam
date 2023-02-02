92142d642e325df826e425dd9278278ff9d70d85
JARS3N <JARS3N@users.noreply.github.com>
2023-02-02 12:50:48 -0500

    Update get_type.R

--- R/get_type.R
+++ R/get_type.R
@@ -10,17 +10,18 @@
 #  }
 #  toupper(out)
 #}
-get_type <- function(doc, inst, file) {
+get_type <- function(doc, inst, file,cal) {
+  # first looks in XML
+  # second infers from file string
+  # third guesses from instrument number
+  # failing all that,just go by wells in calibration
   first <-
     try(unlist(xpathSApply(doc, "//Cartridge//Type", xmlValue)))
-  second <- try(unlist(substr(lot_from_string(file), 1, 1)))
-  if (!is.null(first)) {
-    return(first)
-  } else{
-    if (!is.null(second)) {
-      return(second)
-    } else{
-      return(guess_type_from_inst(inst))
-    }
-  }
+  second <- try(unlist(substr(foam::lot_from_string(file), 1, 1)))
+  third<-foam::guess_type_from_inst(inst)
+ if (!is.null(first) & !is.na(third)) {return(first)}
+ if (!identical(second,character(0)) & !is.na(third)) {return(second)}
+ if(!identical(third,character(0)) & !is.na(third)){return(third)}
+  n<- as.character(nrow(cal)) 
+  unname(c("8"="C","24"="B","96"="W")[n])
 }
