25824795becfc65b5de00e269f9bc476a4e0a82a
JARS3N <JARS3N@users.noreply.github.com>
2023-02-08 18:05:13 -0500

    Update get_cell_counts.R

--- R/get_cell_counts.R
+++ R/get_cell_counts.R
@@ -1,12 +1,8 @@
 get_cell_counts <- function(doc) {
   SessionData <- xpathSApply(doc, "//SessionData", xmlValue)
-  vals <- regmatches(SessionData,
-                     gregexpr("<double>[0-9]+</double>", SessionData))[[1]]
-  num_vals <- as.numeric(gsub("<double>|</double>", "", vals))
-  n <- length(num_vals) / 2 
-  data.frame(
-    Well = plates::num_to_well(1:n,n),
-    Norm_count = num_vals[1:n],
-    Raw_count = num_vals[(n + 1):(n * 2)]
+  tibble(
+    Norm_count = foam::cell_count_vecs(SessionData,"NormaliztionValues"),
+    Raw_count = foam::cell_count_vecs(SessionData,"RawCellCounts"),
+    Well = plates::num_to_well(seq_along(Raw_count),length(Raw_count))
   )
 }
