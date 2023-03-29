49528ac26640e24f1a51f318e47d2c6122e9c053
JARS3N <JARS3N@users.noreply.github.com>
2023-03-29 12:46:58 -0400

    Update munge_pka.R

--- R/munge_pka.R
+++ R/munge_pka.R
@@ -1,4 +1,4 @@
-munge_pka <- function(lvls,type) {
+munge_pka <- function(lvls, type) {
   pH <- c(3.8, 5, 5.8, 6.6, 7.0, 7.4, 8.15, 9.2)
   reps <- unname(c(
     "C" = 1,
@@ -7,32 +7,37 @@ munge_pka <- function(lvls,type) {
     "B" = 3,
     "W" = 12
   )[type])
-  pH_df <- data.frame(pH = unlist(lapply(pH, rep, times = reps)))
-  pH_df$Well <- seq_along(pH_df$pH)
-  last_ticks_of_measures <-
-    unlist(lapply(unique(lvls$Measure), function(u) {
-      max(lvls$Tick[lvls$Measure == u])
-    }))
-  tick_filter <-
-    unlist(rev(lapply(rev(last_ticks_of_measures)[1:2], function(n) {
-      c(-2, -1, 0) + n
-    })))
-  LVL <-
-    lvls[lvls$Tick %in% tick_filter, c("pH_CorrectedEmission", "Tick", "Well", "Measure")]
-  LVL$Tick <- as.numeric(factor(LVL$Tick))
-  X <- merge(LVL, pH_df, by = 'Well')
-  X$Measure <- as.numeric(factor(X$Measure))
-  if (max(X$Measure == 2) & type != "W") {
-    X$dye <- c('CL', 'PR')[X$Measure]
-  } else if (type == "W" & max(X$Measure) == 1) {
-    X$dye <- c(rep("CL", 6), rep("PR", 6))
-  } else{
-    X$dye <- 'CL'
-  }
-
-  setNames(aggregate(
-    pH_CorrectedEmission ~ Well + pH + dye,
-    data = X,
-    FUN = mean
-  ),c("Well", "pH", "dye", "counts"))
+  pH_df <- tibble(pH = unlist(lapply(pH, rep, times = reps)),
+                  Well = seq_along(pH))
+  
+  LVL <- select(lvls, Well, Tick, Measure, pH_CorrectedEmission) %>%
+    merge(pH_df) %>%
+    group_by(Well, Measure, pH) %>%
+    filter(Tick >= (max(Tick) - 2)) %>%
+    summarise(counts = mean(pH_CorrectedEmission)) %>% 
+    ungroup()
+  
+    
+    if (max(LVL$Measure == 2) & type != "W") {
+      LVL %>%
+               left_join(., tibble(
+                 Measure = c(1, 2),
+                 dye = c("CL", "PR")
+               )) %>% select(-Measure)
+    } else if (type == "W" & max(LVL$Measure) == 1) {
+    
+        mutate(
+          LVL,
+          W = plates::num_to_well(Well, 96),
+          Col = plates::cols(W),
+          test = factor(Col > 6, levels = c(F, T)),
+          dye = c("CL", "PR")[test]
+        ) %>%
+        select(-W, -Col, -Measure,-test)
+    } else{
+      mutate(LVL, dye = "CL") %>%
+        select(-Measure)
+    }
+  
+}
 }
