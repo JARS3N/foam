cb48cf2dcb7834300714d2ac53c0850ace79f367
JARS3N <JARS3N@users.noreply.github.com>
2023-05-25 10:56:57 -0400

    Update get_meta.R

--- R/get_meta.R
+++ R/get_meta.R
@@ -1,7 +1,7 @@
 # grab meta data from wave obj
 meta<-function(u){
     dplyr::bind_cols( tibble(Instrument=u$Inst,
-         Lot = u$lot,
+         Lot = paste0(u$type,u$lot),
          sn = u$sn,
          file = u$file
   ) ,as.data.frame(u$software))
