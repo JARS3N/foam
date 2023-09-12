26d5b9635cf5722f010cf924757268bec11ffd16
JARS3N <JARS3N@users.noreply.github.com>
2023-09-12 10:08:02 -0400

    Update analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -1,19 +1,20 @@
 analysis<-function(X){
-  if(X$type=="C" & grepl("wet[ |]qc",X$assay)==T){
+  assay<-tolower(X$assay)
+  if(X$type=="C" & grepl("wet[ |]qc",assay)==T){
     return(X$summary <-foam::combo_assay(X))
   }else{
-    if(X$assay=="ksv"){
+    if(assay=="ksv"){
       return(X$summary <-foam::calc_ksv(X$levels,
                             X$O2_coefs$co,
                             X$calibration
                             ))
     }
-    if(X$assay=="gain"){
+    if(assay=="gain"){
       return(X$summary <-foam::calc_gain(X$levels,
                              X$pH_coefs$target,
                              X$calibration))
     }
-    if(X$assay=="pka"){
+    if(assay=="pka"){
       return(X$summary <- foam::munge_pka(X$levels,X$type))
     }
     # the
