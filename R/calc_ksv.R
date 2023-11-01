b05a929d7606c41244931db41610e7ee922dcc9b
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:32:26 -0400

    Update calc_ksv.R

--- R/calc_ksv.R
+++ R/calc_ksv.R
@@ -6,7 +6,7 @@ calc_ksv<-function(lvls,ppo2,cal){
     mutate(Measure = c("Ambient","F0")[Measure]) %>%
     tidyr::spread(.,Measure,ox) %>%
     mutate(KSV = ((F0/Ambient)-1)/ppo2) %>%
-   suppressMessages( left_join(.,cal) )%>%
+    left_join(.,cal)%>%
     mutate(Gain=NA,sorpH=NA)
 }
 
