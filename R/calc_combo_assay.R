1aab4769b0485dbc7129d82ac659075fec8c8461
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:37:59 -0400

    Update calc_combo_assay.R

--- R/calc_combo_assay.R
+++ R/calc_combo_assay.R
@@ -1,6 +1,6 @@
 combo_assay<-function(X){
   foam::calc_gain(filter(X$levels,Measure==1),X$pH_coefs$target,X$calibration) %>%
-plyr::left_join(
+dplyr::left_join(
    foam::calc_ksv(X$levels,X$O2_coefs$co,X$calibration)
   ) %>%
  dplyr::left_join(.,X$calibration)
