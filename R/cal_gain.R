857c22604fbeda421910d4fe19ffe62ee6d81086
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:44:45 -0400

    Update cal_gain.R

--- R/cal_gain.R
+++ R/cal_gain.R
@@ -2,7 +2,10 @@ calc_gain<- function(lvls, pH_target, cal) {
   filter(lvls, Tick %in% (max(Tick) - c(2, 1, 0))) %>%
     group_by(Well) %>%
     summarize(sorpH = mean(pH_CorrectedEmission)) %>%
-    left_join(., cal) %>%
-    mutate(pH_target = pH_target,Gain = (pH_target / pH.CalEmission) * (1 / 800) * (pH.CalEmission - sorpH)) %>%
-    mutate(KSV = NA,Ambient = NA,F0=NA)
+    left_join(., cal, by = 'Well') %>%
+    mutate(
+      pH_target = pH_target,
+      Gain = (pH_target / pH.CalEmission) * (1 / 800) * (pH.CalEmission - sorpH)
+    ) %>%
+    mutate(KSV = NA, Ambient = NA, F0 = NA)
 }
