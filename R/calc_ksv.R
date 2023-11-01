c0f4e8336da3bba49510184a6db8e633f14a01a9
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:56:37 -0400

    Update calc_ksv.R

--- R/calc_ksv.R
+++ R/calc_ksv.R
@@ -1,13 +1,13 @@
 calc_ksv<-function(lvls,ppo2,cal){
-  select(lvls,Tick,Well,Measure,O2_CorrectedEmission) %>%
-    group_by(Well,Measure) %>%
-    filter(Tick>(max(Tick)-3)) %>%
-    summarise(ox =mean(O2_CorrectedEmission)) %>%
-    mutate(Measure = c("Ambient","F0")[Measure]) %>%
-    tidyr::spread(.,Measure,ox) %>%
-    mutate(KSV = ((F0/Ambient)-1)/ppo2) %>%
-    left_join(.,cal)%>%
-    mutate(Gain=NA,sorpH=NA)
+select(lvls,Tick,Well,Measure,O2_CorrectedEmission) %>%
+  group_by(Well,Measure) %>%
+  filter(Tick>(max(Tick)-3)) %>%
+  summarise(ox =mean(O2_CorrectedEmission),.groups = 'drop') %>%
+  mutate(Measure = c("Ambient","F0")[Measure]) %>%
+  tidyr::spread(.,Measure,ox) %>%
+  mutate(KSV = ((F0/Ambient)-1)/ppo2) %>%
+  left_join(.,cal,by = c("Well", "F0"))%>%
+  mutate(Gain=NA,sorpH=NA)
 }
 
 
