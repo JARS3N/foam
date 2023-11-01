7b211717e0b36243567e946b3cc5f9fa9a8bb692
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:47:23 -0400

    Update calc_O2_lvls.R

--- R/calc_O2_lvls.R
+++ R/calc_O2_lvls.R
@@ -5,7 +5,7 @@ calc_O2_levels <- function(cal, lvls, coefs) {
   cal <- select(cal, Well, O2.CalEmission) %>%
     mutate(F0 = O2.CalEmission * (ppO2 * coefs$Ksv + 1))
 
-  xx <-  left_join(lvls, cal) %>%
+  xx <-  left_join(lvls, cal,by='Well') %>%
     mutate(O2 =  (1 / coefs$Ksv) * ((F0 / O2_CorrectedEmission) - 1))
   xx
 }
