506c44fe2b20966f49c5ac426298ee3de90136ee
JARS3N <JARS3N@users.noreply.github.com>
2023-11-01 14:34:12 -0400

    Update calc_pH_levels.R

--- R/calc_pH_levels.R
+++ R/calc_pH_levels.R
@@ -1,7 +1,7 @@
 calc_pH_levels <- function(cal, lvls, gain) {
   library(dplyr)
   select(
-  mutate(suppressMessages(left_join(lvls, cal, by = "Well")),
+  mutate(left_join(lvls, cal, by = "Well"),
          pH = (7400 - ((pH.CalEmission - pH_CorrectedEmission) / gain
          )) / 1000), Tick, Well,pH)
 }
