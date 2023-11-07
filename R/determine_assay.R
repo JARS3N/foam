58e36856fe0610977c1cccfe0932945ac014c865
JARS3N <JARS3N@users.noreply.github.com>
2023-11-07 10:05:44 -0500

    Update determine_assay.R

--- R/determine_assay.R
+++ R/determine_assay.R
@@ -5,8 +5,8 @@ determine_assay <- function(template,file,force=F,override=NULL){
   }
   a<-unlist(sub_reg(tolower(template),"gain|ksv|barcode|wet qc xfp|wet qc|wetqc|mr|pka|outlier"))
   b<-unlist(sub_reg(tolower(file),"gain|ksv|barcode|wet qc xfp|wet qc|wetqc|mr|pka|outlier"))
-  if(length(a)>0){return(a)}
-  if(length(b>0)){return(b)}else{
+  if(length(a)>0){return(a[1])}
+  if(length(b>0)){return(b[1])}else{
     return(NA)
   }
 }
