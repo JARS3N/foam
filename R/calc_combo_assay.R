9c0128a065fa50ae1ac1cb405f9f9b15f9fd4760
JARS3N <JARS3N@users.noreply.github.com>
2023-11-07 10:45:11 -0500

    Update calc_combo_assay.R

--- R/calc_combo_assay.R
+++ R/calc_combo_assay.R
@@ -1,9 +1,25 @@
-combo_assay<-function(X){
-  foam::calc_gain(filter(X$levels, Measure == 1),
-                  X$pH_coefs$target,
-                  X$calibration) %>%
-    dplyr::left_join(.,
-                     foam::calc_ksv(X$levels, X$O2_coefs$co, X$calibration),
-                     by = 'Well') %>%
-    dplyr::left_join(., X$calibration, by = 'Well')
+
+combo_assay <- function(X) {
+  A <- foam::calc_gain(filter(X$levels, Measure == 1),
+                       X$pH_coefs$target,
+                       X$calibration) %>%
+    select(-KSV, -F0, -Ambient)
+  B <- foam::calc_ksv(X$levels, X$O2_coefs$co, X$calibration) %>%
+    select(-Gain, -sorpH)
+  
+  
+    left_join(
+      A,
+      B,
+      by = join_by(
+        Well,
+        pH.LED,
+        pH.CalEmission,
+        pH.IntialReferenceDelta,
+        pH.Status,
+        O2.LED,
+        O2.CalEmission,
+        O2.IntialReferenceDelta,
+        O2.Status
+      ))
 }
