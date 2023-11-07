dd19c04054fad8453892b583df81ba77a26ee599
JARS3N <JARS3N@users.noreply.github.com>
2023-11-07 11:01:12 -0500

    Update calc_combo_assay.R

--- R/calc_combo_assay.R
+++ R/calc_combo_assay.R
@@ -1,25 +1,31 @@
-
-combo_assay <- function(X) {
-  A <- foam::calc_gain(filter(X$levels, Measure == 1),
-                       X$pH_coefs$target,
-                       X$calibration) %>%
-    select(-KSV, -F0, -Ambient)
-  B <- foam::calc_ksv(X$levels, X$O2_coefs$co, X$calibration) %>%
-    select(-Gain, -sorpH)
-  
-  
-    left_join(
-      A,
-      B,
-      by = join_by(
-        Well,
-        pH.LED,
-        pH.CalEmission,
-        pH.IntialReferenceDelta,
-        pH.Status,
-        O2.LED,
-        O2.CalEmission,
-        O2.IntialReferenceDelta,
-        O2.Status
-      ))
+combo_assay<-function(X){
+  foam::calc_gain(filter(X$levels,Measure==1),X$pH_coefs$target,X$calibration) %>%
+  dplyr::left_join(
+   foam::calc_ksv(X$levels,X$O2_coefs$co,X$calibration)
+  ) %>%
+  dplyr::left_join(.,X$calibration)
 }
+#combo_assay <- function(X) {
+#  A <- foam::calc_gain(filter(X$levels, Measure == 1),
+#                       X$pH_coefs$target,
+#                       X$calibration) %>%
+#   select(-KSV, -F0, -Ambient)
+#  B <- foam::calc_ksv(X$levels, X$O2_coefs$co, X$calibration) %>%
+#    select(-Gain, -sorpH)
+#  
+#  
+#    left_join(
+#      A,
+#      B,
+#      by = join_by(
+#        Well,
+#        pH.LED,
+#       pH.CalEmission,
+#        pH.IntialReferenceDelta,
+#        pH.Status,
+#        O2.LED,
+#        O2.CalEmission,
+#        O2.IntialReferenceDelta,
+#        O2.Status
+#      ))
+#}
